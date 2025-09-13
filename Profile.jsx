28d7ab66cbb0b1897b76d4785987d39969704264
VARSHITJAISWAL <161512294+VARSHITJAISWAL@users.noreply.github.com>
2025-09-13 14:26:46 +0530

    Update Profile.jsx

--- Profile.jsx
+++ Profile.jsx
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router-dom";
-import styles from "./profile.module.css";
+import styles from "./Profile.module.css";
 
 const Profile = () => {
   const navigate = useNavigate();
@@ -104,3 +104,4 @@ const Profile = () => {
 };
 
 export default Profile;
+
